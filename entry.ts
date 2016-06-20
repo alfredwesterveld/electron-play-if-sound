fb9d0fb346982e48a2f051a77a2590a729d4e618
Alfred Westerveld <alfredwesterveld@gmail.com>
2016-06-20 18:46:04 +0200

    HMR

--- entry.ts
+++ entry.ts
@@ -2,23 +2,7 @@
 
 'use strict';
 
-<<<<<<< HEAD
 //const Soundplayer = require('./src/Soundplayer');
 //const soundplayer : typeof Soundplayer  = require('./src/Soundplayer.ts');
 //new soundplayer('./static/0.wav').play();
-alert('foobqqqqq');
-=======
-function isHeadlessElectron() {
-    if (process.platform === 'win32' || process.platform === 'darwin') {
-        return false
-    } else if (process.env.DISPLAY) {
-        const displayNumber = process.env.DISPLAY.slice(1);
-        console.log(displayNumber);
-        return parseInt(displayNumber) >= 99;
-    } else {
-        return true;
-    }
-}
-
-module.exports = isHeadlessElectron;
->>>>>>> 134fda3e6ee9079ad279b12c17eb4ff45f389db8
+console.log('1');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path" )
 
 module.exports = {
-    //entry: path.join(__dirname, "entry.ts"),
     entry: "./entry.ts",
     target: "electron",
     output: {
